b8673c3f37370624f1e70f39dbd7c8ac152b2d55
Oladeji Abdullah <oladejiabdullah17@gmail.com>
2022-08-17 10:30:33 +0100

    Major responsiveness fixed

---
+++ .history/src/components/productList/ProductList_20220611161008.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ProductList.css'
+import Product from '../product/Product'
+import {products} from '../../data'
+
+const ProductList = () => {
+  return (
+    <div className='pL'>
+        <div className="pL-texts">
+            <h1 className="pL-title">Create & inspire. It's Lama</h1>
+            <p className="pL-desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga eaque a mollitia quis maxime optio autem iusto deserunt!.
+            </p>
+        </div>
+        <div className="pL-list">
+            {products.map((item)=> (
+                <Product key={item.id} img={item.picture} link={item.link}/>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
